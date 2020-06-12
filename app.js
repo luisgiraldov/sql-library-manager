33ff17234cda4f22a2f3f9d92ea9d669644861e0
Luis Giraldo <lugirva@gmail.com>
2020-06-12 10:40:01 -0400

    Added logic to update, create and delete books

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("test");
+const express = require('express');
+const path = require('path');
+//importing routes
+const routes = require('./routes/index');
+const books = require('./routes/books');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//setting port for heroku, it takes either the port used by the env or 3000
+const port = process.env.PORT || 3000;
+//middleware to serve the static files in the public folder
+app.use("/static", express.static("public"));
+//To be able to use req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', routes);
+app.use('/books', books);
+
+
+//This middleware will just be responsible for creating the error object and handing off to the error handler (REDIRECTING TO THE 404 ERROR)
+app.use((req,res,next) => {
+    const err = new Error("Not found!");
+    err.status = 404;
+    next(err);
+});
+
+//if an error is passed to the middleware, the middleware will look for this and it will render the error template (THIS IS THE ERROR HANDLER)
+app.use((err, req, res, next)=>{
+    res.locals.error = err;
+    res.locals.image = "/static/images/not-found.jpg";
+    res.status(err.status);
+    res.render("error");
+});
+console.log("App running on port: " + port);
+app.listen(port);
---
+++ routes/books.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+    return async(req, res, next) => {
+      try {
+        await cb(req, res, next)
+      } catch(error){
+        res.status(500).send(error);
+      }
+    }
+}
+
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.render("books/all_books", { books, title: "Books" });
+}));
+
+/** Get New book's form */
+router.get('/new', (req, res) => {
+  res.render("books/new_book", { title: "New Book" });
+});
+
+/* POST create book. */
+router.post('/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books/" + book.id);
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      res.render("books/new_book", { book, errors: error.errors, title: "New Book" })
+    } else {
+      throw error;
+    }  
+  }
+}));
+
+/** Get Book */
+router.get('/:id', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    res.render("books/book_detail", { book, title: book.title });  
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+/* Update a book */
+router.post('/:id', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    if(book) {
+      await book.update(req.body);
+      res.redirect("/books/" + book.id); 
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      book.id = req.params.id;
+      res.render(`books/book_detail`, { book, errors: error.errors, title: "Edit Book" })
+    } else {
+      throw error;
+    }
+  }
+}));
+
+/* Delete book form. */
+router.get("/:id/delete", asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    res.render("books/delete", { book, title: "Delete Book" });
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+/* Delete individual book. */
+router.post('/:id/delete', asyncHandler(async (req ,res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    await book.destroy();
+    res.redirect("/books");
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+
+module.exports = router;
